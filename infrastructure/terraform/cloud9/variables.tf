cdad5140fd63430a4abc8f99191fbafb4fcb2256
Pedro-A-D-S <pedroalves0409@gmail.com>
2023-10-23 08:41:28 -0300

    fix: :lock: Removing secrets from public code

--- infrastructure/terraform/cloud9/variables.tf
+++ infrastructure/terraform/cloud9/variables.tf
@@ -7,11 +7,11 @@ variable "region" {
 variable "aws_access_key_id" {
   type = string
   description = "The AWS access key ID to use."
-  default = "AKIA3DRQDML4P5IYXME5"
+  default = ""
 }
 
 variable "aws_secret_access_key" {
   type = string
   description = "The AWS secret access key to use."
-  default = "oHszpci1N6nJvjPehcFRYXfBfE3rgDgMgMMxyy1D"
+  default = ""
 }
